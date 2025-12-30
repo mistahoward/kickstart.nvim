9ceb6955871d7a608be408d3573dba111d40c2ef
Alex Howard <me@alexhoward.dev>
2025-12-29 22:54:47 -0600

    fix: updating lua to be correct

--- init.lua
+++ init.lua
@@ -897,13 +897,15 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 local rt = require 'rust-tools'
 
-rt.setup {
+rt.setup({
   server = {
     on_attach = function(_, bufnr)
-      vim.keymap.set('n', '<C-space>', rt.hover_actions.hover_actions, { buffer = bufnr })
-      vim.keymap.set('n', '<Leader>a', rt.code_action_group.code_action_group, { buffer = bufnr })
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
     end,
   },
-}:
+})
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
